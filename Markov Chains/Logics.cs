f2158e5def22685a4272a7f0c35c2f4390433b2a
Nabokin Anton Andreevich <anton-nabokin1997@yandex.ru>
2019-03-15 11:06:25 +0300

    Добавлены описания для методов и переменных.

--- Markov Chains/Logics.cs
+++ Markov Chains/Logics.cs
@@ -8,6 +8,43 @@ namespace Markov_Chains
 {
     public class Logics
     {
+        /// <summary>
+        /// Список корневых слов.
+        /// </summary>
+        private List<RootWord> rootWords;
+        /// <summary>
+        /// Количество корневых слов.
+        /// </summary>
+        private int rootWordsCount;
+        /// <summary>
+        /// Максимальный размер матрицы соответствий. Следовательно максимальнкое количество слов для каждого корневого слова.
+        /// </summary>
+        private byte matrixSize;
+        /// <summary>
+        /// Создание класса логики.
+        /// </summary>
+        /// <param name="matrixSize">Максимальный размер матрицы соответствий. Следовательно максимальнкое количество слов для каждого корневого слова.</param>
+        public Logics(byte matrixSize)
+        {
+            rootWords = new List<RootWord>();
+            rootWordsCount = 0;
+            this.matrixSize = matrixSize;
+        }
+        /// <summary>
+        /// Добавление нового корневого слова.
+        /// </summary>
+        /// <param name="word">Строка со словом.</param>
+        public void AddRootWord(string word)
+        {
+            rootWords.Add(new RootWord(word, rootWordsCount++, matrixSize));
+        }
 
+        /// <summary>
+        /// Добавление корневых слов для тестирования.
+        /// </summary>
+        public void AddTestData()
+        {
+            
+        }
     }
 }
--- Markov Chains/RootWord.cs
+++ Markov Chains/RootWord.cs
@@ -6,26 +6,56 @@ using System.Threading.Tasks;
 
 namespace Markov_Chains
 {
+    [Serializable]
     public class RootWord
     {
+        /// <summary>
+        /// Глобальный номер корневого слова.
+        /// </summary>
         public int globalId;
+        /// <summary>
+        /// Локальный номер корневого слова. Всегда равен нулю.
+        /// </summary>
         public int localId;
+        /// <summary>
+        /// Строка со словом.
+        /// </summary>
         public string word;
+        /// <summary>
+        /// Список возможных слов, следующих за корневым.
+        /// </summary>
         public List<NextWord> nextWords;
+        /// <summary>
+        /// Матрица соответствий для данного корневого слова.
+        /// </summary>
         public decimal[,] matrix;
-
-        public RootWord(int globalId, byte matrixSize)
+        /// <summary>
+        /// Создание нового корневого слова.
+        /// </summary>
+        /// <param name="word">Строка со словом.</param>
+        /// <param name="globalId">Глобальный номер корневого слова.</param>
+        /// <param name="matrixSize">Максимальный размер матрицы соответствий.</param>
+        public RootWord(string word, int globalId, byte matrixSize)
         {
+            this.word = word;
             this.globalId = globalId;
             localId = 0;
             nextWords = new List<NextWord>();
             matrix = new decimal[matrixSize,matrixSize];
             ZerosForMatrix(matrixSize);
         }
+        /// <summary>
+        /// Создать новое возможное слово, следующее за корневым.
+        /// </summary>
+        /// <param name="word">Строка со словом.</param>
         public void AddNextWord(string word)
         {
             nextWords.Add(new NextWord(word, nextWords.Count + 1));
         }
+        /// <summary>
+        /// Заполнение матрицы соответствий нулями.
+        /// </summary>
+        /// <param name="matrixSize">Размер матрицы.</param>
         private void ZerosForMatrix(byte matrixSize)
         {
             for (int i = 0; i < matrixSize; i++)
@@ -36,7 +66,10 @@ namespace Markov_Chains
                 }
             }
         }
-        
+        /// <summary>
+        /// Получить массив самых популярных слов.
+        /// </summary>
+        /// <returns>Список самых популярных слов</returns>
         public string[] GetWords()
         {
             string[] words = new string[3];
